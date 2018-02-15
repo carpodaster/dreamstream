36285fc067c9d40ea2f5a95b83a7f37273c24a8f
Carsten Zimmermann <cz@aegisnet.de>
2018-02-15 22:39:26 +0100

    Identify batch name and line number

--- sleepy_record.rb
+++ sleepy_record.rb
@@ -1,7 +1,7 @@
-SleepyRecord = Struct.new(:items_count) do
+SleepyRecord = Struct.new(:name, :items_count) do
   class << self
     def find_in_batches
-      1.upto(4).map { new(10) }.each do |group|
+      1.upto(4).map { |i| new("Batch #{i}", 10) }.each do |group|
         yield group
       end
     end
@@ -9,9 +9,9 @@ SleepyRecord = Struct.new(:items_count) do
 
   def each
     return to_enum(__callee__) unless block_given?
-    items_count.times do
+    items_count.times do |line_no|
       sleep 0.3
-      yield "My;CSV;line\n"
+      yield "#{name};CSV;line #{line_no + 1}\n"
     end
   end
 end
