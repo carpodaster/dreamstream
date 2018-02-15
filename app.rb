11d7f837de83079d4db8a6c51fde10c523774d73
Carsten Zimmermann <cz@aegisnet.de>
2018-02-15 22:11:36 +0100

    Yield to SleepyRecord

--- app.rb
+++ app.rb
@@ -5,11 +5,25 @@ get '/' do
   'Hello Sinatra'
 end
 
+SleepyRecord = Struct.new(:items_count) do
+  class << self
+    def find_in_batches
+      1.upto(4).map { new(10) }
+    end
+  end
+
+  def each
+    items_count.times do
+      sleep 0.3
+      yield "My;CSV;line\n"
+    end
+  end
+end
+
 get '/export' do
   stream do |out|
-    1.upto(10) do
-      out << "Hello world\n"
-      sleep 1
+    SleepyRecord.new(10).each do |line|
+      out << line
     end
     out.flush
   end
